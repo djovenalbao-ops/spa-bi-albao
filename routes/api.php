52aceaacb922ea8edbf0325f505906d2121f0a54
ejay-web-code <ejaydelara1224@gmail.com>
2026-04-15 12:54:15 +0800

    Initial commit

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/sample', function () {
+    return response()->json(['message' => 'This is a sample API endpoint.']);
+});
